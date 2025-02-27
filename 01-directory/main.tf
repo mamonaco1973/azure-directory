dc2510c1666dba25d03959feb0acabdecd017d5b
Mike Monaco <mamonaco1973@gmail.com>
2025-02-27 14:54:00 -0500

    force resource group deletion

--- 01-directory/main.tf
+++ 01-directory/main.tf
@@ -6,6 +6,10 @@ provider "azurerm" {
         purge_soft_delete_on_destroy    = true
         recover_soft_deleted_key_vaults = false
       }
+      
+    resource_group {
+       prevent_deletion_if_contains_resources = false
+       }
     }
 }
 
