4074fdde791484f57cc22289a193833807c09808
Mike Monaco <mamonaco1973@gmail.com>
2025-02-28 13:54:52 -0500

    v1 of join

--- 02-servers/scripts/custom_data.sh
+++ 02-servers/scripts/custom_data.sh
@@ -15,12 +15,11 @@ apt-get update -y
 export DEBIAN_FRONTEND=noninteractive
 
 # Install necessary packages for AD integration, system management, and utilities.
-# - realmd, sssd-ad, sssd-tools: Tools for AD integration and authentication.
+# - realmd, sssd-tools: Tools for AD integration and authentication.
 # - libnss-sss, libpam-sss: Libraries for integrating SSSD with the system.
 # - adcli, samba-common-bin, samba-libs: Tools for AD and Samba integration.
 # - oddjob, oddjob-mkhomedir: Automatically create home directories for AD users.
 # - packagekit: Package management toolkit.
-# - krb5-user: Kerberos authentication tools.
 # - nano, vim: Text editors for configuration file editing.
 apt-get install less unzip realmd sssd-tools libnss-sss \
     libpam-sss adcli samba-common-bin samba-libs oddjob \
