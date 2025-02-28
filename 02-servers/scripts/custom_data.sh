aef128da1e22311951d5d47694caa079a101c848
Mike Monaco <mamonaco1973@gmail.com>
2025-02-28 13:53:30 -0500

    v1 of join

--- 02-servers/scripts/custom_data.sh
+++ 02-servers/scripts/custom_data.sh
@@ -22,6 +22,75 @@ export DEBIAN_FRONTEND=noninteractive
 # - packagekit: Package management toolkit.
 # - krb5-user: Kerberos authentication tools.
 # - nano, vim: Text editors for configuration file editing.
-apt-get install less unzip realmd sssd-ad sssd-tools libnss-sss \
+apt-get install less unzip realmd sssd-tools libnss-sss \
     libpam-sss adcli samba-common-bin samba-libs oddjob \
-    oddjob-mkhomedir packagekit krb5-user nano vim -y
+    oddjob-mkhomedir packagekit nano vim -y
+
+# Write the new sssd.conf
+cat <<EOF > "/etc/sssd/sssd.conf"
+[sssd]
+services = nss, pam
+config_file_version = 2
+domains = mcloud.mikecloud.com
+debug_level = 9
+
+[domain/mcloud.mikecloud.com]
+debug_level = 9
+
+id_provider = ldap
+auth_provider = ldap
+chpass_provider = ldap
+
+# LDAP Connection (Azure AD DS LDAP)
+ldap_uri = ldap://mcloud.mikecloud.com
+
+# Base DN for searches
+ldap_search_base = DC=mcloud,DC=mikecloud,DC=com
+
+# Bind DN (the admin account for LDAP queries)
+ldap_default_bind_dn = CN=mcloud-admin,OU=AADDC Users,DC=mcloud,DC=mikecloud,DC=com
+ldap_default_authtok = F##1r$2%YVhompYlZYBmrpvE
+
+# Search bases for users and groups (can be customized if needed)
+ldap_user_search_base = DC=mcloud,DC=mikecloud,DC=com
+ldap_group_search_base = DC=mcloud,DC=mikecloud,DC=com
+
+# Attribute mapping to Unix fields
+ldap_user_object_class = user
+ldap_user_name = sAMAccountName
+ldap_user_uid_number = uidNumber
+ldap_user_gid_number = gidNumber
+ldap_user_home_directory = unixHomeDirectory
+ldap_user_shell = loginShell
+
+# Explicit attribute mapping (groups)
+ldap_group_object_class = group
+ldap_group_name = sAMAccountName
+ldap_group_gid_number = gidNumber
+ldap_group_member = member
+
+ldap_schema = rfc2307bis
+
+# Use ID mapping (unless you store POSIX attributes in AD)
+ldap_id_mapping = False
+
+# Credential caching (helps with offline logins)
+cache_credentials = True
+
+# Performance tuning
+enumerate = False
+
+# TLS (Optional, but recommended for secure LDAP)
+#ldap_tls_cacert = /etc/sssd/ca.pem   # Point this to the root CA used by your AD DS LDAP if needed
+#ldap_tls_reqcert = allow              # Use "demand" for stricter security if cert is trusted
+
+# Additional options to handle UID/GID if needed (especially if Azure AD DS doesn't have POSIX attributes)
+fallback_homedir = /home/%u
+default_shell = /bin/bash
+EOF
+
+chmod 600 /etc/sssd/sssd.conf
+systemctl stop sssd
+systemctl enable sssd
+systemctl start sssd
+
